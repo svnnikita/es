fa6634502ac61fec81a55cfa1270d6e386b5ab58
svnnikita <svnnikitos@gmail.com>
2025-04-01 17:57:45 +0300

    Объединение веток

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,12 @@ Rcc clock_system;
 
 //==============================================================================
 int main() {
-
-    rcc_periph_clock_enable(RCC_GPIOD);
-
     gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
+
+    rcc_periph_clock_enable(RCC_TIM6);
+    timer_set_prescaler(TIM6, 8000 - 1);
+    timer_set_period(TIM6, 1000 - 1);
+    timer_enable_counter(TIM6);
 
     while (true) {
         gpio_toggle(GPIOD, GPIO15);
