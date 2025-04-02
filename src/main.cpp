69ec657f9c6edcbe774b06ebc41b2f9a35bc594e
svnnikita <svnnikitos@gmail.com>
2025-04-02 17:29:41 +0300

    Лабораторная работа №3: мигание светодиодом при помощи прерываний

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <libopencm3/stm32/rcc.h>   // reset and clock contol
 #include <libopencm3/stm32/gpio.h>  // general purpose input-output
 #include <libopencm3/stm32/timer.h>   // timers
+#include <libopencm3/cm3/nvic.h>
 
 // class Rcc {
 // public:
@@ -14,20 +15,24 @@
 //==============================================================================
 int main() {
     rcc_periph_clock_enable(RCC_GPIOD);
+    rcc_periph_clock_enable(RCC_TIM6);
    
     gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
 
-    rcc_periph_clock_enable(RCC_TIM6);
     timer_set_prescaler(TIM6, 8000 - 1);
-    timer_set_period(TIM6, 1000 - 1);
+    timer_set_period(TIM6, 250 - 1);
+
     timer_enable_counter(TIM6);
 
+    timer_enable_irq(TIM6, TIM_DIER_UIE);
+    nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
+
     while (true) {
-        if (timer_get_counter(TIM6) < 500) {
-            gpio_set(GPIOD, GPIO15);
-        }
-        else {
-            gpio_clear(GPIOD, GPIO15);
-        }
+        
     }
+}
+
+void tim6_dac_isr (void) {
+    timer_clear_flag(TIM6, TIM_SR_UIF);
+    gpio_toggle(GPIOD, GPIO15);
 }
